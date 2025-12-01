b6e415a14ad1744564fd22b986011ad0532bb2a0
Puma1208 <margarita.naryzhnyaya@gmail.com>
2025-12-01 17:00:43 +0100

    Added final logic for calculator - handling operations and variables

--- calculator/calculatorLogic.js
+++ calculator/calculatorLogic.js
@@ -1,18 +1,29 @@
 let screenCalculation = document.querySelector(".focus")
 
+let currentResult = "";
+let operation = "";
+let equals = false;
 
 let numberButtons = document.querySelectorAll(".number");
 for(let i=0; i<numberButtons.length; i++){
-    button = numberButtons[i];
     numberButtons[i].addEventListener("mouseover", function(){
         numberButtons[i].style.backgroundColor='rgba(230, 230, 230, 1)';
     })
     numberButtons[i].addEventListener("mousedown", function(){
         numberButtons[i].style.backgroundColor = 'rgba(194, 194, 194, 1)';
         if (screenCalculation.textContent=="0"){
-            screenCalculation.textContent = ""
+            screenCalculation.textContent = "";
         }
-        screenCalculation.append(numberButtons[i].textContent);
+        if (operation){
+            if (currentResult){
+                screenCalculation.append(numberButtons[i].textContent);
+            }else{
+                currentResult = screenCalculation.textContent;
+                screenCalculation.textContent = numberButtons[i].textContent;
+            }
+        }else{
+            screenCalculation.append(numberButtons[i].textContent);
+        }
     })
     numberButtons[i].addEventListener("mouseup", function(){
         numberButtons[i].style.backgroundColor = '';
@@ -25,18 +36,15 @@ for(let i=0; i<numberButtons.length; i++){
 
 let delButtons = document.querySelectorAll(".del-operation");
 for(let i=0; i<delButtons.length; i++){
-    button = delButtons[i];
     delButtons[i].addEventListener("mouseover", function(){
         delButtons[i].style.backgroundColor='rgba(116, 167, 248, 1)';
     })
     delButtons[i].addEventListener("mousedown", function(){
         delButtons[i].style.backgroundColor = 'rgba(82, 106, 242, 1)';
-        if (screenCalculation.textContent=="0"){
-            screenCalculation.textContent = "0";
-        }
-        else{
+        if (screenCalculation.textContent!="0"){
             if (delButtons[i].textContent=='C'){
                 screenCalculation.textContent = "0";
+                operation = "";
             }
             else if(delButtons[i].textContent=="←"){
                 if (screenCalculation.textContent.length==1){
@@ -55,20 +63,22 @@ for(let i=0; i<delButtons.length; i++){
     })
 }
 
-
 let operationButtons = document.querySelectorAll(".operation");
 for(let i=0; i<operationButtons.length; i++){
-    button = operationButtons[i];
     operationButtons[i].addEventListener("mouseover", function(){
         operationButtons[i].style.backgroundColor='rgba(255, 166, 57, 1)';
     })
     operationButtons[i].addEventListener("mousedown", function(){
         operationButtons[i].style.backgroundColor = 'rgba(194, 94, 1, 1)';
-        if (screenCalculation.textContent=="0"){
-            screenCalculation.textContent = ""
+        if (currentResult){
+            let result = getComputationResult(currentResult, screenCalculation.textContent, operation);
+            screenCalculation.textContent = result.toString();
+            currentResult = "";
+            if (operationButtons[i].textContent == "="){
+                operation = "";
+            }
         }
-        screenCalculation.textContent.
-        // screenCalculation.append(operationButtons[i].textContent);
+        operation = operationButtons[i].textContent;
     })
     operationButtons[i].addEventListener("mouseup", function(){
         operationButtons[i].style.backgroundColor = '';
@@ -77,43 +87,22 @@ for(let i=0; i<operationButtons.length; i++){
         operationButtons[i].style.backgroundColor='';
     })
 }
-// calculationDetails.textContent = computation;
 
 const COMPUTATIONS = ["+", "-", "x", "÷"]
-function getComputationResult(computationString){
-
-    let result = 0;
-    let currentDigits = "";
-    let currentOperation = "";
-
-    for(let i=0; i<computationString.length;i++){
-        if (COMPUTATIONS.includes(computationString[i]) || i == computationString.length-1){
-            if (currentOperation){
-                if (i==computationString.length-1){
-                    currentDigits += computationString[i];
-                }
-                if (currentOperation=="+"){
-                    result += Number(currentDigits);
-                }
-                else if(currentOperation=="-"){
-                    result -= Number(currentDigits);
-                }
-                else if (currentOperation=="x"){
-                    result *= Number(currentDigits);
-                }
-                else if (currentOperation=="÷"){
-                    result /= Number(currentDigits);
-                }
-                else{
-                    return "ERROR: the current operation " + currentOperation + " is not a legal operation.";
-                }
-                currentDigits = "";
-                currentOperation = "";
-            }
-        }
-        else{
-            currentDigits += computationString[i];
-        }
+function getComputationResult(digits1, digits2, operation){
+    if (operation=="+"){
+        return Number(digits1) + Number(digits2);
     }
-    return result
+    else if(operation=="-"){
+        return Number(digits1) - Number(digits2);
+    }
+    else if (operation=="x"){
+        return Number(digits1) * Number(digits2);
+    }
+    else if (operation=="÷"){
+        return Number(digits1) / Number(digits2);
+    }
+    else{
+        return "ERROR: the current operation " + operation + " is not a legal operation.";
+    }
 }
