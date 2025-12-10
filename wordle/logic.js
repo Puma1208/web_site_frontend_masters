d51ccfac2446e217b22b101bf20f69e96d57df46
Puma1208 <margarita.naryzhnyaya@gmail.com>
2025-12-10 15:25:09 +0100

    Re-structured logic to keep track of words

--- wordle/logic.js
+++ wordle/logic.js
@@ -1,29 +1,63 @@
 let letter_boxes = document.querySelectorAll(".input");
 let word_rows = document.querySelectorAll(".row");
-let current_row = 0;
-let next_word_row = word_rows[current_row];
-let next_letter_box = next_word_row.children[0];
 
+let current_row_index = 0;
+let next_letter_box = word_rows[current_row_index].children[0];
 
-let stack_row = [];
+let row_text = "";
+
+const word = "aword".toUpperCase();
 
 function isLetter(el){
     return /^[a-zA-Z]$/.test(el);
 }
 
 document.addEventListener('keydown', function(event){
+  handleKeyBoardInput(event);
+})
+
+function handleKeyBoardInput(event){
   const key = event.key;
+
   if (isLetter(key)){
-    next_letter_box.textContent = key.toUpperCase();
-    updateNextLetterBox();
+    handleNewLetter(key);
+
   }else if (key=="Backspace" || key=="Delete"){
     deletePreviousLetter();
   }
-})
+
+}
+
+
+function handleNewLetter(letter){
+  updateTrackingVariable(letter);
+  updatePointer();
+
+}
+
+function updateTrackingVariable(letter){
+  letter = letter.toUpperCase();
+  next_letter_box.textContent = letter
+  row_text += letter;
+}
+
+function updatePointer(){
+  if (next_letter_box.nextElementSibling==null){
+    if (isRowCorrectWord()){
+    console.log("🌋 YEAY! 🌋")
+  }
+    current_row_index += 1;
+    next_letter_box = word_rows[current_row_index].children[0];
+    row_text = "";
+  }else{
+    next_letter_box = next_letter_box.nextElementSibling;
+  }
+}
 
 function deletePreviousLetter(){
   if (next_letter_box.previousElementSibling != null ){
     current = next_letter_box.previousElementSibling;
+    row_text = row_text.slice(0, -1);
     if (current.previousElementSibling){
       next_letter_box.previousElementSibling.textContent = "";
       next_letter_box = next_letter_box.previousElementSibling;
@@ -34,13 +68,6 @@ function deletePreviousLetter(){
   }
 }
 
-function updateNextLetterBox(){
-  if (next_letter_box.nextElementSibling==null){
-    current_row += 1;
-    next_word_row = word_rows[current_row];
-    next_letter_box = next_word_row.children[0];
-
-  }else{
-    next_letter_box = next_letter_box.nextElementSibling;
-  }
+function isRowCorrectWord(){
+  return (row_text == word);
 }
